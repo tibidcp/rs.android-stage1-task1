24d2cd215b1ddd68f4df0999d6746a451b4732a1
Tibeikin Oleg <tibidcp@gmail.com>
2020-03-22 23:02:43 +0300

    implement HappyArray

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -1,9 +1,23 @@
 package subtask1
 
 class HappyArray {
-
-    // TODO: Complete the following function
     fun convertToHappy(sadArray: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+        return if (sadArray.size <= 2) {
+            sadArray
+        } else {
+            val newArray = sadArray.toMutableList()
+            var bad = newArray.find { !isElementHappy(newArray, it) }
+            while (bad != null) {
+                newArray.remove(bad)
+                bad = newArray.find { !isElementHappy(newArray, it) }
+            }
+            return newArray.toIntArray()
+        }
+    }
+
+    private fun isElementHappy(list: MutableList<Int>, element: Int): Boolean {
+        val index = list.indexOf(element)
+        return index == 0 || index == list.lastIndex
+                || element <= list[index - 1] + list[index + 1]
     }
 }
